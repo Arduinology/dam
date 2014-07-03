2829143899fcb695145ede1a898ec53aa782c739
Heath Naylor <HeathNaylor@gmail.com>
2014-07-03 15:28:51 -0600

    Implment more stuff. DB starting. New dependencies. Switch to QWidget.

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 #dam
-Digital Asset Managment done right. Currently being developed on Windows but will be able to work for Windows, Linux, OSX.
+Digital Asset Management done right. Currently being developed on Windows but will be able to work for Windows, Linux, OSX.
 
 ##Contribute
 Have a better way to implement this? Contact me @arduinology on Twitter or create an issue here.
 
-##Recommended IDE
+##Recommendations
 PyCharm - http://www.jetbrains.com/pycharm/
+pip for Python
 
 ##Dependencies
 PySide - http://qt-project.org/wiki/Category:LanguageBindings::PySide::Downloads
 
 Python For Windows - http://sourceforge.net/projects/pywin32/files/pywin32/
 
-Python 2.7
+Python
+
+PyMongo
+
+MongoDB
--- dam.py
+++ dam.py
@@ -9,43 +9,69 @@ from PySide.QtDeclarative import QDeclarativeView
 import win32com
 import win32com.client
 
-# Create Qt application and the QDeclarative view
-app = QApplication(sys.argv)
-MainWindow = QMainWindow(parent=None)
-MainFrame = QFrame(MainWindow)
-MainFrame.setFrameStyle(QFrame.NoFrame)
-MainWindow.setCentralWidget(MainFrame)
-MainFrameLayout = QVBoxLayout(MainFrame)
+import pymongo
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.test_database
+collection = db.test_collection
 
-view = QDeclarativeView()
-# Create an URL to the QML file
-url = QUrl('views/view.qml')
-# Set the QML file and show
-view.setSource(url)
-label = QLabel()
-label.setFrameStyle(QFrame.Panel | QFrame.Sunken)
-label.setAlignment(Qt.AlignBottom | Qt.AlignLeft)
+class Main(QWidget):
+    def __init__(self):
+        self.borderless = True
+        super(Main, self).__init__()
+        self.initUI()
 
-sh=win32com.client.gencache.EnsureDispatch('Shell.Application',0)
-ns = sh.NameSpace(r'C:\Dropbox\External\Video Tutorials')
-column = 0
-columns = []
-while True:
-    colname=ns.GetDetailsOf(None, column)
-    if not colname:
-        break
-    columns.append(colname)
-    column += 1
+    def initUI(self):
+        uiWidth = 800
+        self.setGeometry(0,0,uiWidth,55)
+        # Create Qt application and the QDeclarative view
+        if self.borderless == True:
+            self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
 
-for item in ns.Items():
-    label.setText(label.text()+"\n")
-    for column in range(len(columns)):
-        colval=ns.GetDetailsOf(item, column)
-        if colval:
-            label.setText(label.text()+"\n"+columns[column]+" | "+colval)
+        ######### User Input *QLineEdit*
+        fileSearch = QLineEdit(self)
+        fileSearch.setFont(QFont('SansSerif', 20))
+        fileSearch.setGeometry(0,0, 770, 25)
+        fsWidth = (uiWidth - fileSearch.width())/2
+        fileSearch.setGeometry(fsWidth,10, 770, 35)
+        fileSearch.textChanged.connect(self.onChanged)
+
+        self.move((QApplication.desktop().availableGeometry().center().x() - self.rect().center().x()),0)
+        label = QLabel()
+        label.setFrameStyle(QFrame.Panel | QFrame.Sunken)
+        label.setAlignment(Qt.AlignBottom | Qt.AlignLeft)
+
+        sh=win32com.client.gencache.EnsureDispatch('Shell.Application',0)
+        ns = sh.NameSpace(r'C:\Dropbox\External\Video Tutorials')
+        column = 0
+        columns = []
+        while True:
+            colname=ns.GetDetailsOf(None, column)
+            if not colname:
+                break
+            columns.append(colname)
+            column += 1
+
+        for item in ns.Items():
+            label.setText(label.text()+"\n")
+            for column in range(len(columns)):
+                colval=ns.GetDetailsOf(item, column)
+                if colval:
+                    label.setText(label.text()+"\n"+columns[column]+" | "+colval)
 
 
-MainFrameLayout.addWidget(label)
-MainWindow.show()
-# Enter Qt main loop
-sys.exit(app.exec_())
+        # MainFrameLayout.addWidget(label)
+        print label.text()
+        self.show()
+        # Enter Qt main loop
+
+    def onChanged(self, text):
+        print(text)
+
+def main():
+    app = QApplication(sys.argv)
+    main = Main()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
